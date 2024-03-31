cf489dcf28991ee1069f6295b03b520063d2f0cb
DuongNguyen9753 <duongnguyen9753@gmail.com>
2024-03-31 22:23:20 +0700

    done the store page

--- js/script.js
+++ js/script.js
@@ -1,15 +1,35 @@
 //login input
 $(document).ready(function(){
     $(".login").click(function(){
-        $(".inputlogin").slideToggle("slow");       
+      $(".inputlogin").slideToggle("slow");      
+  })
+});
+//mo filter 
+$(document).ready(function(){
+  $(".mo-filter-tien").click(function(x){
+    $(".list-filter-tien").slideToggle("slow");
+    $(".mo-filter-tien i").toggleClass("fa-angle-up");
   })
 });
 
-// Get the button
-let mybutton = document.getElementById("topBtn");
-mybutton.style.display = "block";
+$(document).ready(function(){
+  $(".mo-filter-tgphathanh").click(function(x){
+    $(".list-filter-tgphathanh").slideToggle("slow");
+    $(".mo-filter-tgphathanh i").toggleClass("fa-angle-up");
+  })
+});
 
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+$(document).ready(function(){
+  $(".mo-filter-theloai").click(function(x){
+    $(".list-filter-theloai").slideToggle("slow");
+    $(".mo-filter-theloai i").toggleClass("fa-angle-up");
+  })
+});
+
+$(document).ready(function(){
+  $(".mo-filter-nentang").click(function(x){
+    $(".list-filter-nentang").slideToggle("slow");
+    $(".mo-filter-nentang i").toggleClass("fa-angle-up");
+  })
+});
+
